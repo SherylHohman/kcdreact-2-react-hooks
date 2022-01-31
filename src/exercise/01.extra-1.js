9ac5bb6b302ac12ca0ba78b2e8ad1f783b874ed7
Sheryl Hohman <SherylHohman@yahoo.com>
2022-01-30 22:04:01 -0800

    fin exercise-1 and extra-credit-1

--- src/exercise/01.extra-1.js
+++ src/exercise/01.extra-1.js
@@ -1,4 +1,33 @@
 // useState: greeting
 // 💯 accept an initialName
 // http://localhost:3000/isolated/exercise/01.extra-1.js
+// http://localhost:3000/isolated/exercise/01.js
 
+//  Make the Greeting accept a prop called initialName
+//  and initialize the name state to that value.
+
+import * as React from 'react'
+
+function Greeting({initialName = ''}) {
+  const [name, setName] = React.useState(initialName)
+
+  function handleChange(event) {
+    setName(event.target.value)
+  }
+
+  return (
+    <div>
+      <form>
+        <label htmlFor="name">Name: </label>
+        <input value={name} onChange={handleChange} id="name" />
+      </form>
+      {name ? <strong>Hello {name}</strong> : 'Please type your name'}
+    </div>
+  )
+}
+
+function App() {
+  return <Greeting initialName="Susan" />
+}
+
+export default App
