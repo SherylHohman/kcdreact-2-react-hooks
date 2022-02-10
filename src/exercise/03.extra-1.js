e98b610ca66a1c9246be078577040304893dc158
Sheryl Hohman <SherylHohman@yahoo.com>
2022-02-09 19:26:50 -0800

    WIP (bedtime) 03.extra-1: pushing state back down
    ----
     On branch sh
     Changes to be committed:
    	modified:   src/exercise/03.extra-1.js

--- src/exercise/03.extra-1.js
+++ src/exercise/03.extra-1.js
@@ -2,3 +2,56 @@
 // 💯 colocating state
 // http://localhost:3000/isolated/exercise/03.extra-1.js
 
+import * as React from 'react'
+
+/* function Name({name, onNameChange}) { */
+function Name() {
+  const [name, setName] = React.useState('')
+  return (
+    <div>
+      <label htmlFor="name">Name: </label>
+      <input
+        id="name"
+        value={name}
+        onChange={event => setName(event.target.value)}
+      />
+    </div>
+  )
+}
+
+function FavoriteAnimal({animal, onAnimalChange}) {
+  return (
+    <div>
+      <label htmlFor="animal">Favorite Animal: </label>
+      <input id="animal" value={animal} onChange={onAnimalChange} />
+    </div>
+  )
+}
+
+/* function Display({name, animal}) { */
+function Display({animal}) {
+  /* const [animal, setAnimal] = React.useState('') */
+  return <div>{`Hey! Your favorite animal is: ${animal}!`}</div>
+}
+
+function App() {
+  /*
+  // State no longer needed up here, push it back down
+  const [name, setName] = React.useState('')
+  */
+  const [animal, setAnimal] = React.useState('')
+
+  return (
+    <form>
+      {/* <Name name={name} onNameChange={event => setName(event.target.value)} /> */}
+      <Name />
+      <FavoriteAnimal
+        animal={animal}
+        onAnimalChange={event => setAnimal(event.target.value)}
+      />
+      <Display animal={animal} />
+    </form>
+  )
+}
+
+export default App
