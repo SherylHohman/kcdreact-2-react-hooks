7076a57e73baa033dcd638bb28a60ea3073a7cb3
Sheryl Hohman <SherylHohman@yahoo.com>
2022-02-12 17:57:48 -0800

    starting code for 04.extra-1: game to localstorage  On branch sh  Changes to be committed: 	modified:   src/exercise/04.extra-1.js

--- src/exercise/04.extra-1.js
+++ src/exercise/04.extra-1.js
@@ -2,3 +2,125 @@
 // 💯 preserve state in localStorage
 // http://localhost:3000/isolated/exercise/04.extra-1.js
 
+// 04.extra-1 : Preserve State in Local Storage
+//  1. 💯 preserve state in localStorage
+
+//👨‍💼 Our customers want to be able to pause a game, close the tab, and then
+//  resume the game later. Can you store the game’s state in localStorage?
+
+import * as React from 'react'
+
+function Board() {
+  // const resetSquares = () => Array(9).fill(null)    //lazy initialization
+  // const [squares, setSquares] = React.useState(resetSquares) //lazy initialization
+  // no need to use lazy initialization. Not an expensive array fill
+
+  const emptySquares = Array(9).fill(null)
+  const [squares, setSquares] = React.useState(emptySquares)
+
+  const winner = calculateWinner(squares)
+  const nextValue = calculateNextValue(squares)
+  const status = calculateStatus(winner, squares, nextValue)
+
+  // This is the function your square click handler will call. `square` should
+  // be an index. So if they click the center square, this will be `4`.
+    if (squares[square] || Boolean(winner)) {
+      return
+    }
+    //
+    // 🦉 It's typically a bad idea to mutate or directly change state in React.
+    // Doing so can lead to subtle bugs that can easily slip into production.
+    //
+    let newSquares = [...squares]
+    newSquares[square] = nextValue
+    setSquares(newSquares)
+  }
+
+  function restart() {
+    setSquares(emptySquares)
+  }
+
+  function renderSquare(i) {
+    return (
+      <button className="square" onClick={() => selectSquare(i)}>
+        {squares[i]}
+      </button>
+    )
+  }
+
+  return (
+    <div>
+      {/* 🐨 put the status in the div below */}
+      <div className="status">{status}</div>
+      <div className="board-row">
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
+      </div>
+      <div className="board-row">
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
+      </div>
+      <div className="board-row">
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
+      </div>
+      <button className="restart" onClick={restart}>
+        restart
+      </button>
+    </div>
+  )
+}
+
+function Game() {
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board />
+      </div>
+    </div>
+  )
+}
+
+// eslint-disable-next-line no-unused-vars
+function calculateStatus(winner, squares, nextValue) {
+  return winner
+    ? `Winner: ${winner}`
+    : squares.every(Boolean)
+    ? `Scratch: Cat's game`
+    : `Next player: ${nextValue}`
+}
+
+// eslint-disable-next-line no-unused-vars
+function calculateNextValue(squares) {
+  return squares.filter(Boolean).length % 2 === 0 ? 'X' : 'O'
+}
+
+// eslint-disable-next-line no-unused-vars
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ]
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i]
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a]
+    }
+  }
+  return null
+}
+
+function App() {
+  return <Game />
+}
+
+export default App
