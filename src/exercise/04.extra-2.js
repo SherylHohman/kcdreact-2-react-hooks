0d4071e6476f4e5a67d14a28d4660f0012b7f522
Sheryl Hohman <SherylHohman@yahoo.com>
2022-02-13 21:15:58 -0800

    starting code for 04.extra-2: useLocalStorageState
    ----
      On branch sh
      Changes to be committed:
    	modified:   src/exercise/04.extra-2.js

--- src/exercise/04.extra-2.js
+++ src/exercise/04.extra-2.js
@@ -2,3 +2,133 @@
 // 💯 useLocalStorageState
 // http://localhost:3000/isolated/exercise/04.extra-2.js
 
+// 04.extra-2. 💯 useLocalStorageState
+
+// It’s cool that we can get localStorage support with a simple useEffect, but
+//  it’d be even cooler to use the useLocalStorageState hook that’s already
+//  written for us in src/utils.js!
+
+//  Refactor your code to use that custom hook instead.
+//  (This should be a pretty quick extra credit).
+
+import * as React from 'react'
+
+function Board() {
+  const SAVED_GAME = 'TicTacToe'
+  const emptySquares = Array(9).fill(null)
+
+  const [squares, setSquares] = React.useState(() => {
+    return JSON.parse(window.localStorage.getItem(SAVED_GAME)) ?? emptySquares
+  })
+
+  React.useEffect(() => {
+    window.localStorage.setItem(SAVED_GAME, JSON.stringify(squares))
+    return // (no cleanup function to return)
+  }, [squares])
+
+  const winner = calculateWinner(squares)
+  const nextValue = calculateNextValue(squares)
+  const status = calculateStatus(winner, squares, nextValue)
+
+  // This is the function your square click handler will call. `square` should
+  // be an index. So if they click the center square, this will be `4`.
+  function selectSquare(square) {
+    if (winner || squares[square]) {
+      return // early exit, no state change
+    }
+    // 🦉 REM: do NOT mutate or directly change state in React.
+    let newSquares = [...squares]
+    newSquares[square] = nextValue
+    setSquares(newSquares)
+  }
+
+  function restart() {
+    setSquares(emptySquares)
+  }
+
+  function renderSquare(i) {
+    return (
+      <button className="square" onClick={() => selectSquare(i)}>
+        {squares[i]}
+      </button>
+    )
+  }
+
+  return (
+    <div>
+      <div className="status">{status}</div>
+      <div className="board-row">
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
+      </div>
+      <div className="board-row">
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
+      </div>
+      <div className="board-row">
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
+      </div>
+      <button className="restart" onClick={restart}>
+        restart
+      </button>
+    </div>
+  )
+}
+
+function Game() {
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board />
+      </div>
+    </div>
+  )
+}
+
+function calculateStatus(winner, squares, nextValue) {
+  // squares.every(Boolean)
+  // takes the item, passes it in to Boolean to coerce it into truthy or falsey value
+  // so...if every square is true (has an X or O) then this is true, else false
+  return winner
+    ? `Winner: ${winner}`
+    : squares.every(Boolean)
+    ? `Scratch: Cat's game`
+    : `Next player: ${nextValue}`
+}
+
+function calculateNextValue(squares) {
+  // https://michaeluloth.com/filter-boolean
+  // filter(Boolean) filters out falsey objects. In this case null's.
+  // so it counts only X and O's. X ALWAYS goes first. Hence Odd/Even shows who is next.
+  return squares.filter(Boolean).length % 2 === 0 ? 'X' : 'O'
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ]
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i]
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a]
+    }
+  }
+  return null
+}
+
+function App() {
+  return <Game />
+}
+
+export default App
