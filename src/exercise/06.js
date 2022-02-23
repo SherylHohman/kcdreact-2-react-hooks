f973ff110c94dd330e3e57ed906840925069227c
Sheryl Hohman <SherylHohman@yahoo.com>
2022-02-22 18:08:39 -0800

    WIP ~"works" 06:Pokemon: useEffect & http requests
    Mostly works.
    But generates console error message if invalid Pokemon Name is supplied.
    (TODO: Finish tomorrow)
    
    ----
     On branch sh
    	modified:   src/exercise/06.js

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -7,25 +7,65 @@ import * as React from 'react'
 // PokemonInfoFallback: the thing we show while we're loading the pokemon info
 // PokemonDataView: the stuff we use to display the pokemon info
 import {PokemonForm} from '../pokemon'
+import {fetchPokemon, PokemonDataView, PokemonInfoFallback} from '../pokemon'
 
 function PokemonInfo({pokemonName}) {
   // 🐨 Have state for the pokemon (null)
+  const [pokemon, setPokemon] = React.useState(null)
+
   // 🐨 use React.useEffect where the callback should be called whenever the
   // pokemon name changes.
   // 💰 DON'T FORGET THE DEPENDENCIES ARRAY!
-  // 💰 if the pokemonName is falsy (an empty string) then don't bother making the request (exit early).
-  // 🐨 before calling `fetchPokemon`, clear the current pokemon state by setting it to null
-  // 💰 Use the `fetchPokemon` function to fetch a pokemon by its name:
-  //   fetchPokemon('Pikachu').then(
-  //     pokemonData => {/* update all the state here */},
-  //   )
+
+  React.useEffect(() => {
+    // 💰 if the pokemonName is falsy (an empty string) then don't bother making the request (exit early).
+    // 🐨 before calling `fetchPokemon`, clear the current pokemon state by setting it to null
+    // 💰 Use the `fetchPokemon` function to fetch a pokemon by its name:
+    //   fetchPokemon('Pikachu').then(
+    //     pokemonData => {/* update all the state here */},
+    //   )
+
+    const fetchPokemanWrapper = function () {
+      if (!pokemonName) {
+        return // do not fetch, return early
+      }
+      fetchPokemon(pokemonName).then(pokemonData => {
+        /* update all the state here */
+        setPokemon(pokemonData)
+      })
+      // TODO: add catch for the case where the request fails
+    }
+    // TODO: 🐨 before calling `fetchPokemon`, clear the current pokemon state by setting it to null
+    // 💰 Use the `fetchPokemon` function to fetch a pokemon by its name:
+    // fetchPokemon(pokemonName)
+    // .then
+    //     pokemonData => {/* update all the state here */},
+    //   )
+
+    // why? In case of fetch error?
+    setPokemon(null)
+    // It does not seem right to use the setState hook here.
+    // Do I just set it directly here?
+    // Well, maybe setState *is* correct? Resets to loading or unknown value
+    //  until the http request responds? Ponder over it.
+    fetchPokemanWrapper()
+
+    return // no cleanup necessary
+  }, [pokemonName])
+
   // 🐨 return the following things based on the `pokemon` state and `pokemonName` prop:
   //   1. no pokemonName: 'Submit a pokemon'
   //   2. pokemonName but no pokemon: <PokemonInfoFallback name={pokemonName} />
   //   3. pokemon: <PokemonDataView pokemon={pokemon} />
 
   // 💣 remove this
-  return 'TODO'
+  const output = function () {
+    if (!pokemonName) return 'Submit a pokeman'
+    if (!pokemon) return <PokemonInfoFallback name={pokemonName} />
+    return <PokemonDataView pokemon={pokemon} />
+  }
+
+  return <div>{output()}</div>
 }
 
 function App() {
